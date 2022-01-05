34671d233dfd075e688559950b3c8465fe65d64f
Pedro Vicente Santos <pedrovicente@poli.ufrj.br>
2022-01-05 20:02:32 -0300

    test: add test for when rating is incorrect in POST /api/tv-shows

--- tests/api/create-tv-show.spec.js
+++ tests/api/create-tv-show.spec.js
@@ -20,6 +20,7 @@ describe('POST /api/tv-shows endpoint test', () => {
         rating: faker.datatype.number({
           min: 0,
           max: 10,
+          precision: 0.1,
         }),
       };
 
@@ -73,6 +74,66 @@ describe('POST /api/tv-shows endpoint test', () => {
       expect(response.status).to.equal(400);
       expect(response.body.error).to.equal('INVALID_REQUEST');
       expect(response.body.message).to.equal('Invalid request parameters');
+    });
+  });
+
+  context('When sending a invalid tv show rating', () => {
+    let expectedCause = [
+      'rating: Rating must be a number bigger or equal to 0 and equal or smaller than 10',
+    ];
+
+    it('should respond with invalid request if rating is bigger than 10', async () => {
+      const tvShow = {
+        title: faker.lorem.sentence(),
+        description: faker.lorem.paragraph(),
+        rating: 10.1,
+      };
+
+      const response = await testApi
+        .post('/api/tv-shows')
+        .send(tvShow);
+
+      expect(response.status).to.equal(400);
+      expect(response.body.error).to.equal('INVALID_REQUEST');
+      expect(response.body.message).to.equal('Invalid request parameters');
+      expect(response.body.cause).to.deep.equal(expectedCause);
+    });
+
+    it('should respond with invalid request if rating is smaller than 0', async () => {
+      const tvShow = {
+        title: faker.lorem.sentence(),
+        description: faker.lorem.paragraph(),
+        rating: -0.1,
+      };
+
+      const response = await testApi
+        .post('/api/tv-shows')
+        .send(tvShow);
+
+      expect(response.status).to.equal(400);
+      expect(response.body.error).to.equal('INVALID_REQUEST');
+      expect(response.body.message).to.equal('Invalid request parameters');
+      expect(response.body.cause).to.deep.equal(expectedCause);
+    });
+    // Returns the same error message twice because it fails for gte and lte
+    it('should respond with invalid request if rating is not a number', async () => {
+      expectedCause = [
+        'rating: Rating must be a number bigger or equal to 0 and equal or smaller than 10, Rating must be a number bigger or equal to 0 and equal or smaller than 10',
+      ];
+      const tvShow = {
+        title: faker.lorem.sentence(),
+        description: faker.lorem.paragraph(),
+        rating: 'test_rating',
+      };
+
+      const response = await testApi
+        .post('/api/tv-shows')
+        .send(tvShow);
+
+      expect(response.status).to.equal(400);
+      expect(response.body.error).to.equal('INVALID_REQUEST');
+      expect(response.body.message).to.equal('Invalid request parameters');
+      expect(response.body.cause).to.deep.equal(expectedCause);
     });
   });
 
